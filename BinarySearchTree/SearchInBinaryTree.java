31fe12c7173d970af2faa4cfaba38aa3e995b4f8
Shiv kumar <yourshivkumar17@gmail.com>
2026-03-12 11:04:07 +0530

    Searchinbinarytree

---
+++ BinarySearchTree/SearchInBinaryTree.java
@@ -0,0 +1,12 @@
+class Solution {
+    public TreeNode searchBST(TreeNode root, int val) {
+        while(root != null && root.val != val) {
+        if(val < root.val)
+            root = root.left;
+        else
+           root = root.right;
+           
+        }
+        return root;
+    }
+}
