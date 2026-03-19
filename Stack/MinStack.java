64c69fb57a8484388213eda5443d4c53960a04aa
Shiv kumar <yourshivkumar17@gmail.com>
2026-03-19 13:49:35 +0530

    minStack

---
+++ Stack/MinStack.java
@@ -0,0 +1,27 @@
+class MinStack {
+    Stack<Integer> stack;
+    Stack<Integer> minStack;
+
+    public MinStack() {
+        stack = new Stack<>();
+        minStack = new Stack<>();
+    }
+    public void push( int val) {
+        stack.push(val);
+
+        if(minStack.isEmpty())
+            minStack.push(val);
+        else
+            minStack.push(Math.min(val,minStack.peek()));
+    }
+    public void pop() {
+        stack.pop();
+        minStack.pop();
+    }
+    public int top() {
+        return stack.peek();
+    }
+    public int getMin() {
+        return minStack.peek();
+    }
+}
