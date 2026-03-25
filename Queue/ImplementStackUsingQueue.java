b0bbb3160be746e91966c5757de372ab973ab0db
Shiv kumar <yourshivkumar17@gmail.com>
2026-03-25 09:29:47 +0530

    ImplementStackUsingQueue

---
+++ Queue/ImplementStackUsingQueue.java
@@ -0,0 +1,26 @@
+class MyStack {
+    Queue<Integer> q;
+    
+    public MyStack() {
+        q = new LinkedList<>();
+    }
+    
+    public void push(int x) {
+        q.add(x);
+        for(int i=0; i<q.size()-1; i++) {
+            q.add(q.remove());
+        }
+    }
+    
+    public int pop() {
+        return q.remove();
+    }
+    
+    public int top() {
+        return q.peek();
+    }
+    
+    public boolean empty() {
+        return q.isEmpty();
+    }
+}
