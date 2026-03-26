c9becf8d9ad06ac130c02a38cc382703ac4309b0
Shiv kumar <yourshivkumar17@gmail.com>
2026-03-26 11:38:16 +0530

    Valid Anagram

--- String/ValidAnagram.java
+++ String/ValidAnagram.java
@@ -1,30 +1,36 @@
 import java.util.*;
+
 public class ValidAnagram {
     public static void main(String[] args) {
-        Scanner sc = new Scanner (System.in);
+        Scanner sc = new Scanner(System.in);
+
         String s = sc.nextLine();
         String t = sc.nextLine();
-        s = s.toLowerCase();
-        t = t.toLowerCase();
-        s = s.replaceAll("\\s", "");
-        t = t.replaceAll("\\s", "");
 
-        if(s.length() != t.length()) {
+        // lowercase + remove spaces (optional)
+        s = s.toLowerCase().replaceAll("\\s", "");
+        t = t.toLowerCase().replaceAll("\\s", "");
+
+        if (s.length() != t.length()) {
             System.out.println(false);
             return;
         }
-        int[] count = new int[256];
-        for(int i=0; i<s.length(); i++) {
-            count[s.charAt(i)]++;
-            count[t.charAt(i)]--;
+
+        int[] count = new int[26];  // sirf a-z ke liye
+
+        for (int i = 0; i < s.length(); i++) {
+            count[s.charAt(i) - 'a']++;  // index 0-25
+            count[t.charAt(i) - 'a']--;
         }
-        for(int i=0; i<256; i++) {
-            if(count[i] != 0) {
-                System.out.println("false");
+
+        for (int i = 0; i < 26; i++) {
+            if (count[i] != 0) {
+                System.out.println(false);
                 return;
             }
         }
-         System.out.println("true");
+
+        System.out.println(true);
         sc.close();
     }
 }
