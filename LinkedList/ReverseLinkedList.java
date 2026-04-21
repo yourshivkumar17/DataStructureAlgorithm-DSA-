a16dc1e757567e1447cf9948f35a10fbfb25bb81
Shiv kumar <yourshivkumar17@gmail.com>
2026-04-21 15:29:25 +0530

    Reverse LinkedList

---
+++ LinkedList/ReverseLinkedList.java
@@ -0,0 +1,12 @@
+public class ReverseLinkedList {
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        while(head != null) {
+            ListNode temp = head.next;
+            head.next = prev;
+            prev = head;
+            head = temp;
+        }
+        return prev;
+    }
+}
